7e154f4a446d92583c9bbdc1a76cb98cbdda8208
ReynoldArun09 <reynold.seq@gmail.com>
2026-07-09 12:45:11 +0530

    chore: all pages dynamic

--- frontend-admin/test-api.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-axios.get('http://localhost:3000/api/v1/admin/reports/investments').then(res => {
-  console.log("Response data keys:", Object.keys(res.data));
-  console.log("Is res.data.data undefined?", res.data.data === undefined);
-}).catch(err => console.error(err.message));
